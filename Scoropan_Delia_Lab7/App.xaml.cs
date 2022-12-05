1d0ba90e5b3261b0bb0f366c930eb2248951e610
Delia Scoropan <delia.scoropan@yahoo.com>
2022-12-05 22:21:48 +0200

    Lab8

--- Scoropan_Delia_Lab7/App.xaml.cs
+++ Scoropan_Delia_Lab7/App.xaml.cs
@@ -1,11 +1,28 @@
 ﻿namespace Scoropan_Delia_Lab7;
-
+using System;
+using Scoropan_Delia_Lab7.Data;
+using System.IO;
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
-}
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+    }
